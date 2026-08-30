dee05fa78efef0e1ae121cb3b37a287d4a7e84ad
afeez-07 <afeez8288@gmail.com>
2026-08-30 11:38:48 +0530

    Initial commit - Devmind AI

---
+++ backend/devmind-backend/src/test/java/com/devmind/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmind.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
